d232df7e9cc2fc6882d858d2d4c7d953ff70c6e9
Meaghan Campbell <emeaghancampbell@gmail.com>
2021-03-19 21:39:57 -0500

    post routes

--- models/Post.js
+++ models/Post.js
@@ -15,12 +15,9 @@ Post.init(
             type: DataTypes.STRING,
             allowNull: false
         },
-        post_url: {
-            type: DataTypes.STRING,
+        post_content: {
+            type: DataTypes.TEXT,
             allowNull: false,
-            validate: {
-                isURL: true
-            }
         },
         user_id: {
             type: DataTypes.INTEGER,
