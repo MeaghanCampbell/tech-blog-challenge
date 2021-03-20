742c4fd793094dbf088cf0b44b0fe64fa0104d51
Meaghan Campbell <emeaghancampbell@gmail.com>
2021-03-19 21:57:16 -0500

    comment routes

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -1,4 +1,31 @@
 const router = require('express').Router();
 const { Comment } = require('../../models');
 
+
+// get all comments
+router.get('/', (req, res) => {
+
+});
+
+
+// post a comment
+router.post('/', (req, res) => {
+    Comment.create({
+        comment_text: req.body.comment_text,
+        user_id: req.body.user_id,
+        post_id: req.body.post_id
+    })
+    .then(dbCommentData => res.json(dbCommentData))
+    .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+    });
+});
+
+
+// delete a comment
+router.delete('/:id', (req, res) => {
+
+});
+
 module.exports = router
