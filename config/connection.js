107881ce3262b493f04fb25bc7b14531aa140570
Meaghan Campbell <emeaghancampbell@gmail.com>
2021-03-19 20:28:55 -0500

    models & user routes

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,12 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+require('dotenv').config()
+
+let sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     host: 'localhost',
     dialect: 'mysql',
     port: 3306
-});  
+});
+
   
 module.exports = sequelize;
