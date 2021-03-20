32339151dee4ce46cd7f5345ef4a5b2983d1f224
Meaghan Campbell <emeaghancampbell@gmail.com>
2021-03-20 13:10:02 -0500

    login logout

--- server.js
+++ server.js
@@ -22,6 +22,21 @@ const hbs = exphbs.create({});
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
+// connect session
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+app.use(session(sess));
+
+// start server & listen on port
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log(`Now listening on port ${PORT}`))
 })
